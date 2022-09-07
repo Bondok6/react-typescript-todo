c373a02b9ea738788a893b7a1a6b70c2314c19a0
Kyrillos Hany <kyrillos.hany@hamza.solutions>
2022-09-07 22:59:35 +0200

    Fix darg and drop issue

--- src/App.tsx
+++ src/App.tsx
@@ -19,34 +19,41 @@ const App = () => {
   };
 
   const handleOnDragEnd = (result: DropResult) => {
-    const { source, destination } = result;
-    if (!destination) return;
+    const { destination, source } = result;
+
+    if (!destination) {
+      return;
+    }
+
     if (
       destination.droppableId === source.droppableId &&
       destination.index === source.index
-    )
+    ) {
       return;
+    }
 
-    let add,
-      active = todos,
-      completed = completedTodos;
+    let add;
+    let active = todos;
+    let complete = completedTodos;
 
+    // Source Logic
     if (source.droppableId === 'TodosList') {
       add = active[source.index];
       active.splice(source.index, 1);
     } else {
-      add = completed[source.index];
-      completed.splice(source.index, 1);
+      add = complete[source.index];
+      complete.splice(source.index, 1);
     }
 
+    // Destination Logic
     if (destination.droppableId === 'TodosList') {
       active.splice(destination.index, 0, add);
     } else {
-      completed.splice(destination.index, 0, add);
+      complete.splice(destination.index, 0, add);
     }
 
+    setCompletedTodos(complete);
     setTodos(active);
-    setCompletedTodos(completed);
   };
 
   return (
